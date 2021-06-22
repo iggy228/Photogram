e55df993b10c06bf985a756ca4e891dac8177f12
iggy228 <i.borovy@gmail.com>
2021-06-22 18:49:57 +0200

    Add title for register and form srollable

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -21,42 +21,44 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.fromLTRB(16.0, 20.0, 16.0, 0.0),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              'Photogram',
-              style: TextStyle(
-                fontSize: 64.0,
-                fontFamily: 'DancingScript'
+      child: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text(
+                'Photogram',
+                style: TextStyle(
+                  fontSize: 64.0,
+                  fontFamily: 'DancingScript'
+                ),
+                textAlign: TextAlign.center,
               ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 16.0),
-            // username field
-            TextFormField(
-              decoration: InputDecoration(hintText: 'Username'),
-              onChanged: (String val) => setState(() => username = val),
-            ),
-            SizedBox(height: 8.0),
-            // password field
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(hintText: 'Password'),
-              onChanged: (String val) => setState(() => password = val),
-            ),
-            SizedBox(height: 8.0),
-            OutlinedButton(
-              onPressed: () {},
-              child: Text('Submit'),
-            ),
-            TextButton(
-              onPressed: widget.showRegisterHandler,
-              child: Text('Register'),
-            ),
-          ],
+              SizedBox(height: 16.0),
+              // username field
+              TextFormField(
+                decoration: InputDecoration(hintText: 'Username'),
+                onChanged: (String val) => setState(() => username = val),
+              ),
+              SizedBox(height: 8.0),
+              // password field
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(hintText: 'Password'),
+                onChanged: (String val) => setState(() => password = val),
+              ),
+              SizedBox(height: 8.0),
+              OutlinedButton(
+                onPressed: () {},
+                child: Text('Submit'),
+              ),
+              TextButton(
+                onPressed: widget.showRegisterHandler,
+                child: Text('Register'),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/auth/register.dart
+++ lib/screens/auth/register.dart
@@ -26,57 +26,68 @@ class _RegisterState extends State<Register> {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.fromLTRB(16.0, 20.0, 16.0, 0.0),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            // username field
-            TextFormField(
-              validator: validateUsername,
-              decoration: InputDecoration(hintText: 'Username'),
-              onChanged: (String val) => setState(() => username = val),
-            ),
-            SizedBox(height: 8.0),
-            // email field
-            TextFormField(
-              validator: validateEmail,
-              decoration: InputDecoration(hintText: 'E-mail'),
-              onChanged: (String val) => setState(() => email = val),
-            ),
-            SizedBox(height: 8.0),
-            // password field
-            TextFormField(
-              obscureText: true,
-              validator: validatePassword,
-              decoration: InputDecoration(hintText: 'Password'),
-              onChanged: (String val) => setState(() => password = val),
-            ),
-            SizedBox(height: 8.0),
-            // confirm password field
-            TextFormField(
-              obscureText: true,
-              validator: validatePassword,
-              decoration: InputDecoration(hintText: 'Repeat password'),
-              onChanged: (String val) => setState(() => confirmPassword = val),
-            ),
-            SizedBox(height: 8.0),
-            OutlinedButton(
-              onPressed: () async {
-                if (_formKey.currentState!.validate()) {
-                  if (password != confirmPassword) return;
+      child: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text(
+                'Register',
+                style: TextStyle(
+                    fontSize: 64.0,
+                    fontFamily: 'DancingScript'
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 24),
+              // username field
+              TextFormField(
+                validator: validateUsername,
+                decoration: InputDecoration(hintText: 'Username'),
+                onChanged: (String val) => setState(() => username = val),
+              ),
+              SizedBox(height: 8.0),
+              // email field
+              TextFormField(
+                validator: validateEmail,
+                decoration: InputDecoration(hintText: 'E-mail'),
+                onChanged: (String val) => setState(() => email = val),
+              ),
+              SizedBox(height: 8.0),
+              // password field
+              TextFormField(
+                obscureText: true,
+                validator: validatePassword,
+                decoration: InputDecoration(hintText: 'Password'),
+                onChanged: (String val) => setState(() => password = val),
+              ),
+              SizedBox(height: 8.0),
+              // confirm password field
+              TextFormField(
+                obscureText: true,
+                validator: validatePassword,
+                decoration: InputDecoration(hintText: 'Repeat password'),
+                onChanged: (String val) => setState(() => confirmPassword = val),
+              ),
+              SizedBox(height: 8.0),
+              OutlinedButton(
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    if (password != confirmPassword) return;
 
-                  UserId? userId = await AuthService().register(email, password);
-                  print(userId);
-                }
-              },
-              child: Text('Submit'),
-            ),
-            TextButton(
-              onPressed: widget.showLoginHandler,
-              child: Text('Login'),
-            ),
-          ],
+                    UserId? userId = await AuthService().register(email, password);
+                    print(userId);
+                  }
+                },
+                child: Text('Submit'),
+              ),
+              TextButton(
+                onPressed: widget.showLoginHandler,
+                child: Text('Login'),
+              ),
+            ],
+          ),
         ),
       ),
     );
